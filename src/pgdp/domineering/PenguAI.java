a0a985582c1d896d4f8795e7caf10c737d7aca35
Johannes Stöhr <johannes.stoehr@tum.de>
2020-12-21 13:05:28 +0100

    Add name

--- src/pgdp/domineering/PenguAI.java
+++ src/pgdp/domineering/PenguAI.java
@@ -2,6 +2,7 @@ package pgdp.domineering;
 
 public class PenguAI extends AI {
     public static final boolean PARTICIPATING = false;
+    public static final String NAME = null;
 
     @Override
     public synchronized Coordinate playMove(char[][] board, Player player, Mode mode) {
