235bf6aa929ca5ca3d7be6a893e7aa85874db426
ahayler <a.hayler@gmx.de>
2020-12-24 01:00:17 +0100

    Test first MinMaxAI

--- src/pgdp/domineering/HumanPlayer.java
+++ src/pgdp/domineering/HumanPlayer.java
@@ -3,6 +3,8 @@ package pgdp.domineering;
 public class HumanPlayer extends AI {
     @Override
     public Coordinate playMove(char[][] board, Player player, Mode mode) {
+        int x = MiniJava.readInt("x-Koordinate: ");
+        int y
         return null;
     }
 }
--- src/pgdp/domineering/PenguAI.java
+++ src/pgdp/domineering/PenguAI.java
@@ -6,7 +6,8 @@ public class PenguAI extends AI {
     @Override
     public synchronized Coordinate playMove(char[][] board, Player player, Mode mode) {
         /*return MinMaxAI.getMove(board, player, 1);*/
-        return AdvancedCostFunctionAI.getMove(board, player);
+        /*return AdvancedCostFunctionAI.getMove(board, player);*/
+        return SimpleCostFunctionAI.getMove(board, player);
         /*return MinMaxAI.getMove(board, player, 1);*/
     }
 }
