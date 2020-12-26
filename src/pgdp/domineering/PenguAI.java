1822e8b60cc95f7f96036bf0af74cde8487e6b34
ahayler <a.hayler@gmx.de>
2020-12-26 02:01:57 +0100

    Test AlphaBeta Pruning

--- src/pgdp/domineering/PenguAI.java
+++ src/pgdp/domineering/PenguAI.java
@@ -11,7 +11,7 @@ public class PenguAI extends AI {
 
     private AI EasyAI = new MinMaxAI(1, new RealAndSafeMovesEvaluationFunction(), false, false, 0, false);
     private AI MediumAI = new MinMaxAI(2, new RealAndSafeMovesEvaluationFunction(), true, true, 5, true);
-    private AI HardAI = new MinMaxAI(3, new RealAndSafeMovesEvaluationFunction(), true, true, 13, true);
+    private AI HardAI = new MinMaxAI(3, new RealAndSafeMovesEvaluationFunction(), true, true, 12, true);
 
     @Override
     public synchronized Coordinate playMove(char[][] board, Player player, Mode mode) {
