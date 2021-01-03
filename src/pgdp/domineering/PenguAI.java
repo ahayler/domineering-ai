b84df10874db61200ab7a7a538772455c9670454
ahayler <a.hayler@gmx.de>
2021-01-03 23:39:45 +0100

    Test V2

--- src/pgdp/domineering/PenguAI.java
+++ src/pgdp/domineering/PenguAI.java
@@ -1,6 +1,7 @@
 package pgdp.domineering;
 
 import pgdp.domineering.ai.MinMaxAI_V2;
+import pgdp.domineering.ai.MinMaxAI_V1;
 import pgdp.domineering.evaluation_function.ExtendedRealAndSafeMovesEvaluationFunction;
 import pgdp.domineering.evaluation_function.RealAndSafeMovesEvaluationFunction;
 
@@ -13,7 +14,7 @@ public class PenguAI extends AI {
             false, 0, true);
     private AI MediumAI = new MinMaxAI_V2(2, new ExtendedRealAndSafeMovesEvaluationFunction(), true,
             true, 10, true);
-    private AI HardAI = new MinMaxAI_V2(3, new ExtendedRealAndSafeMovesEvaluationFunction(), true,
+    private AI HardAI = new MinMaxAI_V1(3, new ExtendedRealAndSafeMovesEvaluationFunction(), true,
             false, 11, true);
 
     @Override
