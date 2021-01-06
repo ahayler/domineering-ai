6b0540852581a3f5d6c8ef5e8bdd32df5f00a47b
ahayler <a.hayler@gmx.de>
2021-01-06 18:32:59 +0100

    v0.1: losing against D2V

--- src/pgdp/domineering/ai/MinMaxAI_V3.java
+++ src/pgdp/domineering/ai/MinMaxAI_V3.java
@@ -39,7 +39,7 @@ public class MinMaxAI_V3 extends AI {
         this.useAlphaBeta = useAlphaBeta;
 
         this.turnNumber = 0;
-        hashtable = new Hashtable<String, int[]>(2000);
+        hashtable = new Hashtable<String, int[]>(800);
     }
 
     @Override
