765acc758cd4f3fc61b3db6dbf980d6208875ed2
ahayler <a.hayler@gmx.de>
2021-01-07 21:59:39 +0100

    v0.2: added OldReworkedFunction; losing against D4V_Ex and D4H_Old

--- src/pgdp/domineering/PenguAI.java
+++ src/pgdp/domineering/PenguAI.java
@@ -11,7 +11,6 @@ public class PenguAI extends AI {
 
     public static final boolean PARTICIPATING = true;
 
-
     private AI EasyAI = new MinMaxAI_V2(2, new RealAndSafeMovesEvaluationFunction(), true,
             false, 0, true, false);
     private AI MediumAI = new MinMaxAI_V2(2, new ExtendedRealAndSafeMovesEvaluationFunction(), true,
