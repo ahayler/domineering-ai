035d0884098ad6c1c8af79d1b96a8478382089e8
ahayler <a.hayler@gmx.de>
2021-01-07 21:51:03 +0100

    v0.2: added OldReworkedFunction; losing against D4V_Ex and D4H_Old

--- src/pgdp/domineering/SimulateGame.java
+++ src/pgdp/domineering/SimulateGame.java
@@ -1,9 +1,6 @@
 package pgdp.domineering;
 
-import pgdp.domineering.ai.HumanPlayerWithAITakeOver;
-import pgdp.domineering.ai.MinMaxAI_V2;
-import pgdp.domineering.ai.MinMaxAI_V1;
-import pgdp.domineering.ai.MinMaxAI_V3;
+import pgdp.domineering.ai.*;
 import pgdp.domineering.evaluation_function.ExtendedRealAndSafeMovesEvaluationFunction;
 import pgdp.domineering.evaluation_function.ReworkedEvaluationFunction;
 
@@ -16,9 +13,8 @@ public class SimulateGame {
         MinMaxAI_V3 horizontalAI =  new MinMaxAI_V3(4, new ReworkedEvaluationFunction(), true,
                 false, 13, true, true);
 
-        Game game = new Game(new MinMaxAI_V2(4, new ExtendedRealAndSafeMovesEvaluationFunction(), true,
-                false, 13, true, false),
-                new HumanPlayerWithAITakeOver(horizontalAI), Mode.EASY);
+        Game game = new Game(new HumanPlayer(),
+                horizontalAI, Mode.EASY);
         game.runGame();
         System.out.println(game.getWinner());
     }
---
+++ src/pgdp/domineering/evaluation_function/OldReworkedEvaluationFunction.java
@@ -0,0 +1,28 @@
+package pgdp.domineering.evaluation_function;
+
+public class OldReworkedEvaluationFunction extends EvaluationFunction {
+    /*
+    The main part is still Real + Safe Moves (difference between players); but the second order value is now
+    Safe Move Possibilities - DeadMoves (difference between players)
+    */
+    @Override
+    public int[] evaluate(int verticalMobility, int horizontalMobility, int verticalRealMoves, int horizontalRealMoves,
+                          int verticalSafeMoves, int horizontalSafeMoves, int verticalSafeMovePossibilities,
+                          int horizontalSafeMovePossibilities, int freeMovesVertical, int freeMovesHorizontal) {
+
+        int verticalDeadMoves = verticalRealMoves - verticalSafeMoves
+                - verticalSafeMovePossibilities - freeMovesVertical;
+        int horizontalDeadMoves = horizontalRealMoves - horizontalSafeMoves
+                - horizontalSafeMovePossibilities - freeMovesHorizontal;
+
+        return new int[]{verticalRealMoves + verticalSafeMoves
+                - horizontalRealMoves - horizontalSafeMoves,
+                (verticalSafeMovePossibilities - verticalDeadMoves)
+                        - (horizontalSafeMovePossibilities - horizontalDeadMoves)};
+    }
+
+    @Override
+    public boolean mobilityNeeded() {
+        return false;
+    }
+}
